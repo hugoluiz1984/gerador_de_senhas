7fc9972e46fce72ee559305176959d95c072262b
hugoluiz1984 <57529085+hugoluiz1984@users.noreply.github.com>
2023-06-02 14:02:25 -0300

    Adicionado que tipo de caracter pode ser utilizado
    Adicionado que tipo de caracter pode ser utilizado

--- src/GeradorSenhas.jsx
+++ src/GeradorSenhas.jsx
@@ -1,18 +1,27 @@
-import { Button, BottomNavigation,BottomNavigationAction, Slider } from '@mui/material';
-import React, { useState } from 'react'
+import { Button, BottomNavigation,BottomNavigationAction, Slider, Switch, FormControl, FormLabel, FormControlLabel, FormGroup } from '@mui/material';
+import React, { useEffect, useState } from 'react'
 
 const GeradorSenhas = () => {
     const [senha, setSenha] = useState('');
     const [tamanho, setTamanho] = useState(10);
     const [primeira, setPrimeira] = useState('1');
-    const [fLetter, setFLetter] = useState('');
+    const [cMai, setCMai] = useState(true);
+    const [cMin, setCMin] = useState(true);
+    const [cNum, setCNum] = useState(true);
+    const [cEsp, setCEsp] = useState(true);
     const min = 'abcdefghijklmnopqrstuvwxyz';
     const mai = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const num = '0123456789'
+    const num = '0123456789';
+    const esp = '!@#$%^&*()';
+    const [clip, setClip] = useState(false);
     
+    useEffect(() => {
+        if (!cMai && !cMin && !cNum && !cEsp) setCMin(true);
+
+    }, [cMai, cMin, cNum, cEsp]);
 
     const gerarSenha = (length) => {
-        var letra
+        let letra
         switch (primeira) {
             case '1':
                 const MinGerada = Array.from({ length: 1 }, () => min[Math.floor(Math.random() * min.length)]).join('');
@@ -28,10 +37,16 @@ const GeradorSenhas = () => {
                 break;
         }
     // Lógica para gerar a senha
-    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
-    const senhaGerada = Array.from({ length: length }, () => caracteres[Math.floor(Math.random() * caracteres.length)]).join('');
-        
+        let caracteres = '';
+        if (cMai) caracteres = caracteres + mai;
+        if (cMin) caracteres = caracteres + min;
+        if (cNum) caracteres = caracteres + num;
+        if (cEsp) caracteres = caracteres + esp;
+        console.log(caracteres);
+    //const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+    const senhaGerada = Array.from({ length: length }, () => caracteres[Math.floor(Math.random() * caracteres.length)]).join(''); 
     setSenha(letra+senhaGerada);
+    setClip(false);
     };
     
     
@@ -65,17 +80,47 @@ const GeradorSenhas = () => {
                 valueLabelDisplay="auto"
                 onChange={(_, newValue) => setTamanho(newValue)}
               />
-              <Button variant="contained" onClick={() => gerarSenha(tamanho-1)}>Gerar Senha</Button>
-              
+              <FormControl component="fieldset" variant="standard">
+                <FormLabel component="legend">Tipos de caracteres:</FormLabel>
+                <FormGroup>
+                    <FormControlLabel
+                    control={
+                        <Switch checked={cMai} onChange={() => { setCMai(!cMai)}} name="mai" />
+                    }
+                    label="Maiúsculas"
+                    />
+                    <FormControlLabel
+                    control={
+                        <Switch checked={cMin} onChange={() => { setCMin(!cMin)}} name="min" />
+                    }
+                    label="Minúscula"
+                    />
+                    <FormControlLabel
+                    control={
+                        <Switch checked={cNum} onChange={() => { setCNum(!cNum)}} name="Num" />
+                    }
+                    label="Numeral"
+                      />
+                    <FormControlLabel
+                    control={
+                        <Switch checked={cEsp} onChange={() => { setCEsp(!cEsp)}} name="mai" />
+                    }
+                    label="Especiais"
+                    />
+                </FormGroup>
+              </FormControl>
+                
           </div>
+                        <Button variant="contained" onClick={() => gerarSenha(tamanho-1)}>Gerar Senha</Button>
           <div>
               <p>{senha}</p>
               <Button
                   disabled={senha==''?true:false}
-                  onClick={() => { navigator.clipboard.writeText(senha) }}
+                  onClick={() => { navigator.clipboard.writeText(senha);setClip(true) }}
               >
                   Copiar
               </Button>
+                <p>{clip&& 'Senha copiada para o clipboard'}</p>
               </div>
         
     </div>
